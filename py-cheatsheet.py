1f72d2f61368d9da1eb0c53c1d3a1185963f9a94
Thinh Dat Nguyen <ntdhat@outlook.com>
2020-04-26 20:56:33 -0700

    refactor: Print out arithmetic operators

--- py-cheatsheet.py
+++ py-cheatsheet.py
@@ -3,7 +3,9 @@ import pandas           # Regular import
 import pandas as pd     # Import with an alias
 from math import  sqrt  # Import submodule of a module
 
-# Operators: +, -, *, /, **, //, %
+# Operators:
+print('Arithmetic: +, -, *, /, **, //, %')
+print('Assignment: =, +=, *=')
 
 # Variables & Assignment Operators
 
