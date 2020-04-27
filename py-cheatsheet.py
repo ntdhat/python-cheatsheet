ee3cf2ad898d504731f367b3612e17b8bec9da9f
Thinh Dat Nguyen <ntdhat@outlook.com>
2020-04-26 18:07:41 -0700

    feat: Add Operators

--- py-cheatsheet.py
+++ py-cheatsheet.py
@@ -2,3 +2,5 @@
 import pandas           # Regular import
 import pandas as pd     # Import with an alias
 from math import  sqrt  # Import submodule of a module
+
+# Operators: +, -, *, /, **, //, %
